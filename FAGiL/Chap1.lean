442f2cc6a5577cf9127a2f735437135a2d946791
Cminorwhy <112066107+Cminorwhy@users.noreply.github.com>
2025-01-07 13:16:07 +0800

    Fill the instances of category of sets and category of modules.

--- FAGiL/Chap1.lean
+++ FAGiL/Chap1.lean
@@ -1,10 +1,10 @@
 /-
-Copyright (c) 2024 Nantao Zhang. All rights reserved.
+Copyright (c) 2024 Nantao Zhang, Hongyu Wang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Nantao Zhang
 -/
 import Mathlib.Tactic
-
+import Mathlib.Algebra.Category.ModuleCat.Basic
 
 universe u u₁ v
 
@@ -22,15 +22,32 @@ variable {α: Type u₁}
 /-
 (1.1.2) Set is a category.
 -/
-instance : Category (Set α) where
+
+instance : Category Type where
   Hom X Y := X → Y
-  id X := id
-  comp f g := fun a ↦ g (f a)
+  id X := fun x => x
+  comp f g := fun x => g (f x)
+  id_comp := by intros; rfl
+  comp_id := by intros; rfl
+  assoc := by intros; rfl
 
 /-
 (1.1.3) Vect_k is a category.
 -/
-
+variable (R : Type u) [Ring R]
+instance : Category (ModuleCat R) where
+  Hom X Y := LinearMap (RingHom.id R) X Y
+  id X := LinearMap.id
+  comp f g := LinearMap.comp g f
+  id_comp := by
+    intros
+    rfl
+  comp_id := by
+    intros
+    rfl
+  assoc := by
+    intros
+    rfl
 
 /-
 (1.1.A) The groupoid is defined by Mathlib.CategoryTheory.Groupoid.Groupoid
